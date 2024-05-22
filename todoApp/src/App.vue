dee3c2c8175e2f6a62ff60d80a645f434a182f67
rjzin <bigriann18@gmail.com>
2024-05-22 15:57:09 -0300

    Update
    Update em estilos. Refinando tela!

--- todoApp/src/App.vue
+++ todoApp/src/App.vue
@@ -5,9 +5,14 @@
 
     <!-- Header -->
     <div class="flex flex-row mb-6">
-      <form @submit.prevent="addTask">
-        <input v-model="todo" class=" py-2 px-2 rounded-xl text-center  inset-10 border-x-8 outline-offset-0 outline-none" type="text" placeholder="Insira sua task aqui...">
-        <button type="submit" class=" active:scale-90 text-white py-2 px-2 ml-2 font-bold bg-sky-500 hover:bg-sky-700 rounded-xl w-16">+</button>
+      <form @submit.prevent="addTask" class="justify-center items-center flex flex-row">
+        <input id="ipt_todo" v-model="todo" class=" py-2 px-2 rounded-xl text-center  inset-10 border-x-8 outline-offset-0 outline-none" type="text" placeholder="Insira sua task aqui..." required maxlength="60">
+        <button type="submit" class=" active:scale-90 text-white py-2 px-2 ml-2 font-bold bg-sky-500 hover:bg-sky-700 rounded-xl">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+              <path fill-rule="evenodd" d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z" clip-rule="evenodd" />
+
+          </svg>
+        </button>
       </form>
     </div>
     
@@ -16,7 +21,18 @@
     <div class="container box-border border-solid border-l-4 border-sky-200 mb-4 w-96 h-1/2 overflow-y-auto custom-scrollbar">
 
       <ul class="text-white font-semibold capitalize mt-5 m-5">
-        <li class="flex flex-row justify-between items-center" v-for="task in todos" :key="task.id"> {{task.name}} <button class="ml-3 py-1 px-1 mx-1 my-1 bg-rose-500 rounded-xl w-9 h-6 justify-center  text-center items-center">-</button> </li>
+        <li class="flex flex-row justify-between items-center mb-6" v-for="task in todos" :key="task.id"> 
+          
+
+          <strong>{{task.name}} </strong>
+
+          <button class="ml-3 py-1 px-1 mx-1 my-1 bg-rose-500 rounded-xl  justify-center  text-center items-center active:scale-90 hover:bg-rose-800 text-white">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+  <path fill-rule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+</svg>
+
+          </button> 
+        </li>
       </ul>
 
 
@@ -48,6 +64,9 @@ function addTask() {
 </script>
 
 <style scoped>
+
+
+/*Custom scrollbar o codigo foi copiado da internet*/
 /* Custom scrollbar styles for WebKit browsers (Chrome, Safari) */
 .custom-scrollbar::-webkit-scrollbar {
   width: 12px;
@@ -72,5 +91,9 @@ function addTask() {
 .custom-scrollbar {
   scrollbar-width: thin;
   scrollbar-color: #888 #2c2c2c; /* Thumb color and Track color */
+}
+
+#ipt_todo::placeholder{
+  font-weight: 600;
 }
 </style>
